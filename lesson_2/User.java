1d54abd147e42f7f9db91b1d31af2467a96cd4c7
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2021-06-04 06:06:26 +0900

    Create User.java

---
+++ lesson_2/User.java
@@ -0,0 +1,14 @@
+public class User {
+
+	public static void main(String[] args) {
+		//전부 프린터기를 사용해서 출력해보세요.
+		
+		Printer pt_1 = new Printer();
+		pt_1.powerOn();
+		pt_1.print("출력테스트1");
+		pt_1.print("출력테스트2");
+		pt_1.print("출력테스트3");
+		pt_1.print("출력테스트4");
+		pt_1.print("출력테스트5");
+	}
+}
