60c31c023915823469abb5492f853c867953973c
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2021-06-04 21:32:01 +0900

    Create Ink.java

---
+++ compare_1/Ink.java
@@ -0,0 +1,14 @@
+package lesson_1;
+
+public class Ink {
+
+	private int inkCount;
+	
+	public Ink() {
+		inkCount = 3;
+	}
+	
+	public void useInk() {
+		System.out.println("남은 잉크 수량: " + --inkCount);
+	}
+}
