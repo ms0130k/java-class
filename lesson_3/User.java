d6d027adf8690ebfeb127551fcf2bd9470b8cd8a
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2021-06-08 17:36:56 +0900

    Create User.java

---
+++ lesson_3/User.java
@@ -0,0 +1,15 @@
+package lesson_3;
+
+public class User {
+
+	public static void main(String[] args) {
+		Printer pt = new Printer(true);
+		pt.checkPower();
+		
+		pt.print("냐하하");
+		pt.print("냐하하");
+		pt.print("냐하하");
+		pt.print("냐하하");
+	}
+
+}
